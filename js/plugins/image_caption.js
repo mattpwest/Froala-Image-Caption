4496e3b0a96a45d28c4fc7b7e47211760f76bc8b
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-02-02 17:54:21 +0200

    First pass at a functionally equivalent replacement for Froala 2.4.1.

--- js/plugins/image_caption.js
+++ js/plugins/image_caption.js
@@ -1,129 +1,207 @@
 /*!
  * Froala Image Caption Plugin
- * Written by Matt Dziuban (http://mattdziuban.com)
- * Tested with Froala v1.2.3 - v1.2.6 (http://editor.froala.com)
+ *
+ * Originally written by Matt Dziuban (http://mattdziuban.com) for Froala v1.2.3 - v1.2.6 (http://editor.froala.com)
+ * Updated by Matt Van Der Westhuizen (https://chaotik.co.za) for Froala v2.4.1
  */
 (function ($) {
-  $.Editable.DEFAULTS = $.extend($.Editable.DEFAULTS, {
-    imageCaption: true
-  });
-
-  $.Editable.VALID_NODES = $.merge($.Editable.VALID_NODES, ['FIGURE','FIGCAPTION']);
-
-  $.Editable.prototype.initImageCaptionEvents = function() {
-    var that = this;
-    this.$element.on('click touchend', 'img:not([contenteditable="false"])', function(e) {
-      if ($(this).closest('.thumbnail').length > 0)
-        var caption = $(this).closest('.thumbnail').find('.caption').html();
-      else
-        var caption = '';
-      that.$image_editor.find('.f-image-caption textarea').val(caption);
+    $.extend($.FroalaEditor.POPUP_TEMPLATES, {
+        'imageCaptionPlugin.popup': '[_BUTTONS_][_CUSTOM_LAYER_]'
     });
-    this.$element.on('drop', function(e) {
-      var $img = that.$element.find('.fr-image-dropped');
-      if ($img.length == 0 || !$img.attr('data-ref-id') || $($img.data('ref-id')).length == 0)
-        return;
-      var $captionContainer = $($img.data('ref-id'));
-      $captionContainer.remove();
-      $captionContainer.find('img').remove();
-      $captionContainer.find('.thumbnail').prepend($img.get(0).outerHTML);
-      $img.replaceWith($captionContainer);
+
+    $.FroalaEditor.DEFAULTS = $.extend($.FroalaEditor.DEFAULTS, {
+        imageCaption: {
+            debug: true,
+            popupButtons: ['imageBack', '|']
+        }
     });
-  };
 
-  var originalAddImageClass = $.Editable.prototype.addImageClass;
-  $.Editable.prototype.addImageClass = function($obj, cls) {
-    originalAddImageClass($obj, cls);
-    if (!this.options.imageCaption)
-      return;
-    if ($obj.closest('.thumbnail').length > 0) {
-      originalAddImageClass($obj.closest('.thumbnail'), cls);
-      $obj.closest('.thumbnail').find('.caption').css('width', ($obj.attr('width')-18)+'px')
-    }
-  };
+    $.FroalaEditor.PLUGINS.imageCaptionPlugin = function(currentEditor) {
+        var editor = currentEditor;
+        var opts = editor.opts.imageCaption;
 
-  $.Editable.prototype.addCaptionField = function() {
-    $('<div class="f-popup-line f-image-caption">')
-        .append('<label><span data-text="true">Caption</span>: </label>')
-        .append($('<textarea class="caption-input"></textarea>').on('mouseup keydown', function(e) {
-          var keyCode = e.which;
-          if (!keyCode || keyCode !== 27)
-            e.stopPropagation();
-        }))
-        .append('<button class="f-ok" style="float:right!important;" data-text="true" data-callback="setImageCaption" data-cmd="setImageCaption title="OK">OK</button>')
-        .appendTo(this.$image_editor);
-  };
+        function _init() {
+            _log('Image Caption Plugin initialized with options: ', opts);
+        }
 
-  $.Editable.prototype.bindKeyboardCaptionDeletionHandler = function() {
-    $('.froala-element').on('keydown', function(e) {
-      if (e.keyCode == 8 || e.keyCode == 46) {
-        $(this).find('.thumbnail .caption').each(function(i, caption) {
-          if ($.trim($(caption).text()) === '') {
-            var $img = $(caption).closest('.thumbnail').find('img');
-            $(caption).closest('.thumbnail').replaceWith('<p>'+$img.get(0).outerHTML+'</p>');
-          }
-        });
-      }
+        function _initPopup () {
+            // Popup buttons.
+            var popup_buttons = '';
+
+            // Create the list of buttons.
+            if (editor.opts.imageCaption.popupButtons.length > 0) {
+                popup_buttons += '<div class="fr-buttons">';
+                popup_buttons += editor.button.buildList(editor.opts.imageCaption.popupButtons);
+                popup_buttons += '</div>';
+            }
+
+            // Load popup template.
+            var templateHtml = '<div class="fr-image-caption-layer fr-layer fr-active" id="fr-image-caption-layer-' + editor.id + '">' +
+                                  '<div class="fr-input-line">' +
+                                    '<input id="fr-image-caption-layer-text-' + editor.id + '" type="text" placeholder="' + editor.language.translate("Caption Text") + '" tabIndex="1">' +
+                                  '</div>' +
+                                  '<div class="fr-action-buttons">' +
+                                    '<button type="button" class="fr-command fr-submit" data-cmd="imageSetCaption" tabIndex="2" role="button">'
+                                      + editor.language.translate("Update") +
+                                    '</button>' +
+                                  '</div>' +
+                                '</div>';
+            var template = {
+                buttons: popup_buttons,
+                custom_layer: templateHtml
+            };
+
+            // Create popup.
+            var $popup = editor.popups.create('imageCaptionPlugin.popup', template);
+
+            // Assign refresh handler.
+            editor.popups.onRefresh('imageCaptionPlugin.popup', function () {
+                console.log ('refresh');
+
+                var $popup = editor.popups.get('imageCaptionPlugin.popup');
+                var $image = $(editor.image.get()[0]);
+
+                if ($image) {
+                    $popup.find('input').val($image.parent().find('.caption').text()).trigger('change');
+                }
+            });
+
+            // Assign hide handler.
+            editor.popups.onHide('imageCaptionPlugin.popup', function () {
+                console.log ('hide');
+            });
+
+            return $popup;
+        }
+
+        function showPopup () {
+            var $popup = editor.popups.get('imageCaptionPlugin.popup');
+
+            if (!$popup) {
+                $popup = _initPopup();
+            }
+
+            editor.popups.setContainer('imageCaptionPlugin.popup', editor.$sc);
+
+            editor.popups.refresh('imageCaptionPlugin.popup');
+
+            // Use the image.edit plugin's popup as the base position for the caption popup
+            var $base = editor.popups.get('image.edit');
+
+            var left = $base.position().left + $base.outerWidth() / 2;
+            var top = $base.position().top;
+
+            // Show the custom popup
+            editor.popups.show('imageCaptionPlugin.popup', left, top, $base.outerHeight());
+        }
+
+        function hidePopup () {
+            editor.popups.hide('imageCaptionPlugin.popup');
+        }
+
+        function setImageCaption() {
+            var $image = $(editor.image.get()[0]);
+            var $popup = editor.popups.get('imageCaptionPlugin.popup');
+            var $input = $popup.find('input');
+            var captionText = $input.val();
+
+            // Insert / update content
+            if ($.trim(captionText) === '') {
+                if ($image.closest('.thumbnail').length > 0)
+                    $image.closest('.thumbnail').parent().parent().html($image.get(0).outerHTML);
+            } else {
+                if ($image.closest('.thumbnail').length > 0) {
+                    $image.closest('.thumbnail').find('.caption').text(captionText);
+                } else {
+                    var classes = '' + this.getImageClass($image.attr('class'));
+                    var refId = 'img-' + (new Date()).getTime();
+                    var captionHtml =
+                        '<span class="post-caption-container ' + classes + '" id="' + refId + '">' +
+                          '<span class="thumbnail">' +
+                            $image.attr('data-ref-id', '#' + refId).get(0).outerHTML + //.replace('>', 'contenteditable="true">') +
+                            '<span class="caption pull-center" style="width:' + ($image.width() - 18) + 'px" contenteditable="false">' + captionText +
+                            '</span>' +
+                          '</span>' +
+                        '</span>';
+
+                    $image.parent().html(captionHtml);
+                }
+            }
+
+            // Hide this popup and show the original image editing popup
+            editor.popups.hide('imageCaptionPlugin.popup');
+            editor.popups.show('image.edit');
+        }
+
+        function getImageClass(cls) {
+            if (!cls)
+                return 'fr-fi';
+            var classes = cls.split(' ');
+            if (classes.indexOf('fr-fir') >= 0)
+                return 'fr-fir';
+            if (classes.indexOf('fr-fil') >= 0)
+                return 'fr-fil';
+            return 'fr-fi';
+        }
+
+        function _log() {
+            if (!opts.debug) {
+                return;
+            }
+
+            var args = Array.prototype.slice.call(arguments);
+            console.log.apply(console, args);
+        }
+
+        return {
+            _init: _init,
+            showPopup: showPopup,
+            hidePopup: hidePopup,
+            setImageCaption: setImageCaption,
+            getImageClass: getImageClass
+        }
+    };
+
+    $.FroalaEditor.DefineIcon('header', {NAME: 'header', template: 'font_awesome'});
+    $.FroalaEditor.RegisterCommand('imageCaption', {
+        title: 'Caption',
+        icon: 'header',
+        undo: true,
+        focus: true,
+        showOnMobile: true,
+        refreshAfterCallback: true,
+        callback: function() {
+            console.log('imageCaption button pressed...');
+            this.imageCaptionPlugin.showPopup();
+        },
+        refresh: function($btn) {
+            console.log('imageCaption button state changed: ', this.selection.element());
+        }
     });
-  };
 
-  var originalRemoveImage = $.Editable.prototype.removeImage;
-  $.Editable.prototype.removeImage = function($img) {
-    if (!this.options.imageCaption)
-      return originalRemoveImage.call(this, $img);
-    var $image_editor = this.$element.find('span.f-img-editor'),
-      message = 'Are you sure? Image will be deleted.';
-    if ($.Editable.LANGS[this.options.language]) {
-      message = $.Editable.LANGS[this.options.language].translation[message];
-    }
-    if (!confirm(message))
-      return $image_editor.find('img').click();
-    if ($image_editor.length === 0) return false;
-    var $img_parent = $image_editor.parents('.thumbnail');
-    originalRemoveImage.call(this, $img);
-    if ($img_parent.length > 0)
-      $img_parent.eq(0).remove();
-  };
-
-  $.Editable.prototype.setImageCaption = function() {
-    var $image = this.$element.find('span.f-img-editor img'),
-      captionText = this.$image_editor.find('.f-image-caption textarea').val();
-    this.hide();
-    this.closeImageMode();
-    if ($.trim(captionText) === '') {
-      if ($image.closest('.thumbnail').length > 0)
-        $image.closest('.thumbnail').replaceWith('<p>'+$image.get(0).outerHTML+'</p>');
-    } else {
-      if ($image.closest('.thumbnail').length > 0)
-        $image.closest('.thumbnail').find('.caption').text(captionText);
-      else {
-        var classes = 'thumbnail clearfix '+this.getImageClass($image.attr('class'));
-        var refId = 'img-'+(new Date()).getTime();
-        var captionHtml = '<div class="post-caption-container" id="'+refId+'"><figure class="'+classes+'">'+$image.attr('data-ref-id', '#'+refId).get(0).outerHTML
-          +'<figcaption class="caption pull-center" contenteditable="false" style="width:'+($image.attr('width')-18)+'px">'+captionText
-          +'</figcaption></figure></div>';
-        if ($image.parent().children().length > 1)
-          $image.replaceWith(captionHtml);
-        else
-          $image.parent().replaceWith(captionHtml);
-      }
-    }
-  };
-
-  $.Editable.prototype.initCaptions = function() {
-    if (!this.options.imageCaption)
-      return;
-    this.options.imageDeleteConfirmation = false;
-    this.initImageCaptionEvents();
-    this.addCaptionField();
-    this.bindKeyboardCaptionDeletionHandler();
-    this.$original_element.on('editable.imageResize', function(e) {
-      var $imgEditor = $('.f-img-editor');
-      if ($imgEditor.closest('.thumbnail').length == 0)
-        return;
-      $imgEditor.closest('.thumbnail').find('.caption').css('width', ($imgEditor.find('img').attr('width')-18)+'px');
+    $.FroalaEditor.RegisterCommand('imageSetCaption', {
+        title: 'Update',
+        undo: true,
+        focus: false,
+        showOnMobile: true,
+        refreshAfterCallback: false,
+        callback: function() {
+            console.log('imageCaption button pressed...');
+            this.imageCaptionPlugin.setImageCaption();
+        },
+        refresh: function($btn) {
+            console.log('imageCaption button state changed: ', this.selection.element());
+        }
     });
-  };
 
-  $.Editable.initializers.push($.Editable.prototype.initCaptions);
+    $.FroalaEditor.DefineIcon('popupClose', { NAME: 'times', template: 'font_awesome' });
+    $.FroalaEditor.RegisterCommand('popupClose', {
+        title: 'Close',
+        icon: 'times',
+        undo: false,
+        focus: false,
+        callback: function () {
+            this.imageCaptionPlugin.hidePopup();
+        }
+    });
 })(jQuery);
